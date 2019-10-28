e5fd707d121f0b74cf6404ec3a87ef1ed7eb541e
Jonathan Souza <jonathan.wdev@gmail.com>
2019-10-28 20:03:49 -0300

    adicionado arquivo insomnia

--- api/src/app/controllers/AvaliableController.js
+++ api/src/app/controllers/AvaliableController.js
@@ -17,7 +17,7 @@ class AvaliableController {
       res.status(400).json({ error: 'Invalid date' });
     }
     const searchDate = Number(date);
-    const appointments = await Appointment.finddAll({
+    const appointments = await Appointment.findAll({
       where: {
         provider_id: req.params.providerId,
         canceled_at: null,
